1ecc4656bdd2b97ff7b0651442dee4cc3e0b2dbe
Pedro Carvalho <pedro.carvalho@wooba.com.br>
2024-08-20 18:02:04 -0300

    Update

--- Tela.cs
+++ Tela.cs
@@ -14,21 +14,37 @@ namespace gitTeste
         {
             Users dadosUsuarios = new Users();
 
-            Console.WriteLine($"\nDados do Usuário\n");
-            Console.Write("Nome Usuário: ");
-            dadosUsuarios.Nome = Console.ReadLine();
+            /*while ()
+            {
+                Console.WriteLine($"\nDados do Usuário\n");
+                Console.Write("Nome Usuário: ");
+                dadosUsuarios.Nome = Console.ReadLine();
 
-            Console.Write("CPF Usuário: ");
-            dadosUsuarios.Cpf = Console.ReadLine();
-            dadosUsuarios.validacaoCpf();
+                Console.Write("CPF Usuário: ");
+                dadosUsuarios.Cpf = Console.ReadLine();
+                dadosUsuarios.validacaoCpf();
 
-            Console.Write("Idade Usuário: ");
-            dadosUsuarios.Idade = int.Parse(Console.ReadLine());
-            dadosUsuarios.validarIdade();
+                Console.Write("Idade Usuário: ");
+                dadosUsuarios.Idade = int.Parse(Console.ReadLine());
+                dadosUsuarios.validarIdade();
 
-            listaCadastros.Add(dadosUsuarios);
+                listaCadastros.Add(dadosUsuarios);
+            }*/
+            
 
             gerarListaUsuarios(listaCadastros);
+        }
+
+        public string seguirCadastro()
+        {
+            Console.Write("Deseja cadastrar mais usuários: S/N");
+            var seguirCadastro = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(seguirCadastro) && seguirCadastro.ToUpper() == "S")
+            {
+                return seguirCadastro;
+            }
+            return seguirCadastro;
         }
 
         public void gerarListaUsuarios(List<Users> listaCadastros)
@@ -36,16 +52,19 @@ namespace gitTeste
             foreach (var user in listaCadastros)
             {
                 Console.WriteLine($"Nome: {user.Nome}");
-                Console.WriteLine($"CPF: {user.Cpf}");
+
+                if (user.Cpf != null)
+                {
+                    Console.WriteLine($"CPF: {user.Cpf}");
+                }
                 Console.WriteLine($"Idade: {user.Idade}");
-                break;
             }
-            
+
         }
 
         public void Response()
         {
-            
+
         }
     }
 }
