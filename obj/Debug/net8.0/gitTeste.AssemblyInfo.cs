172e89ac7836b29dd956546c9bc8eb819ecc8438
BDevOne <vedb91@gmail.com>
2025-01-09 16:29:13 -0300

    Teste develop

--- obj/Debug/net8.0/gitTeste.AssemblyInfo.cs
+++ obj/Debug/net8.0/gitTeste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gitTeste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7ee650dc20dca5992cb5fac591834d2ee3acef8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423285cae9d7a3c60db5eb7e5acd0f2c9045eb20")]
 [assembly: System.Reflection.AssemblyProductAttribute("gitTeste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gitTeste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
